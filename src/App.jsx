30877210ec5baac5e343da898d9a83a672bcb1cf
Simon Babatunde <orlhatundji@gmail.com>
2024-12-28 15:03:34 +0100

    fix blocks background coverin elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import About from './sections/About'
-import Contact from './sections/Contact'
-import Experience from './sections/Experience'
-import Footer from './sections/Footer'
-import Hero from './sections/Hero'
-import Navbar from './sections/Navbar'
-import Projects from './sections/Projects'
-import Testimonials from './sections/Testimonials'
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Experience from "./sections/Experience";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+import Projects from "./sections/Projects";
+import Testimonials from "./sections/Testimonials";
 
 const App = () => {
   return (
     <>
-     
-    <main className='max-w-7xl mx-auto'>
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Testimonials />
-      <Experience />
-      <Contact />
-      <Footer />
-    </main>
+      <div class="blocks-container">
+        <div id="blocks"></div>
+      </div>
+      <main className="max-w-7xl mx-auto">
+        <Navbar />
+        <Hero />
+        <About />
+        <Projects />
+        <Testimonials />
+        <Experience />
+        <Contact />
+        <Footer />
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
