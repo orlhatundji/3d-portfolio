dcddd8444a2d792c253d1fe7e605b11f33b8c305
Simon Babatunde <orlhatundji@gmail.com>
2024-12-28 13:28:06 +0100

    update featured projects section

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,32 +1,32 @@
 export const navLinks = [
   {
     id: 1,
-    name: 'Home',
-    href: '#hero',
+    name: "Home",
+    href: "#hero",
   },
   {
     id: 2,
-    name: 'About',
-    href: '#about',
+    name: "About",
+    href: "#about",
   },
   {
     id: 3,
-    name: 'Work',
-    href: '#work',
+    name: "Work",
+    href: "#work",
   },
   {
     id: 4,
-    name: 'Contact',
-    href: '#contact',
+    name: "Contact",
+    href: "#contact",
   },
 ];
 
 export const clientReviews = [
   {
     id: 1,
-    name: 'Joseph Godwin',
-    position: 'Software engineer at SQUIRE',
-    img: 'assets/joseph.jpeg',
+    name: "Joseph Godwin",
+    position: "Software engineer at SQUIRE",
+    img: "assets/joseph.jpeg",
     rating: 5,
     review: `
       Simon is an exceptional Software Engineer. 
@@ -36,195 +36,164 @@ export const clientReviews = [
       This, combined with his resourcefulness, excellent teamwork 
       and problem-solving skills, makes him an invaluable asset to any engineering team. 
       It’s always a pleasure working with him, and I highly recommend.
-    `
-  }
+    `,
+  },
 ];
 
 export const myProjects = [
   {
-    title: 'Podcastr - AI Podcast Platform',
-    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
-    subdesc:
-      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
-    texture: '/textures/project/project1.mp4',
-    logo: '/assets/project-logo1.png',
+    title: "Entrepreneurship and Skill Development Center",
+    desc: `University of Lagos has one of the oldest Business Faculties in Africa established 
+    in 1962 and has produced numerous Entrepreneurs who are captains of industries in many 
+    sectors of the Nigerian economy and Africa.`,
+    subdesc: `Built with Next.js, TailwindCSS, and Framer Motion, the Entrepreneurship and Skill Development Center
+    website is a platform that showcases the center's activities, programs, and events.`,
+    href: "#",
+    texture: "/textures/project/project1.mp4",
+    logo: "/assets/project-logo1.png",
     logoStyle: {
-      backgroundColor: '#2A1816',
-      border: '0.2px solid #36201D',
-      boxShadow: '0px 0px 60px 0px #AA3C304D',
+      backgroundColor: "#2A1816",
+      border: "0.2px solid #36201D",
+      boxShadow: "0px 0px 60px 0px #AA3C304D",
     },
-    spotlight: '/assets/spotlight1.png',
+    spotlight: "/assets/spotlight1.png",
     tags: [
       {
         id: 1,
-        name: 'React.js',
-        path: '/assets/react.svg',
+        name: "Next.js",
+        path: "/assets/nextjs.svg",
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
       },
       {
         id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
+        name: "TypeScript",
+        path: "/assets/typescript.png",
       },
       {
         id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
+        name: "Framer Motion",
+        path: "/assets/framer.png",
       },
     ],
   },
   {
-    title: 'LiveDoc - Real-Time Google Docs Clone',
-    desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
-    subdesc:
-      'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
-    href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
-    texture: '/textures/project/project2.mp4',
-    logo: '/assets/project-logo2.png',
+    title: "Ololade Ayelabola",
+    desc: `A talented model, beauty queen, entrepreneur, and philanthropist. 
+    She has won many beauty pageants and turned her love for fashion into successful businesses. Ololade is also deeply committed to helping others through her charity work. 
+    Discover her inspiring journey and see how she makes a difference in the world.`,
+    subdesc: `
+      Built with Gatsby.js, TailwindCSS, and Framer Motion, the Ololade Ayelabola website is a platform that showcases Ololade's journey, achievements, and philanthropic work.
+    `,
+    href: "https://www.ololadeayelabola.ng",
+    texture: "/textures/project/project2.mp4",
+    logo: "/assets/project-logo2.png",
     logoStyle: {
-      backgroundColor: '#13202F',
-      border: '0.2px solid #17293E',
-      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      backgroundColor: "#13202F",
+      border: "0.2px solid #17293E",
+      boxShadow: "0px 0px 60px 0px #2F6DB54D",
     },
-    spotlight: '/assets/spotlight2.png',
+    spotlight: "/assets/spotlight2.png",
     tags: [
       {
         id: 1,
-        name: 'React.js',
-        path: '/assets/react.svg',
+        name: "React.js",
+        path: "/assets/react.svg",
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
       },
       {
         id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
+        name: "TypeScript",
+        path: "/assets/typescript.png",
       },
       {
         id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
+        name: "Framer Motion",
+        path: "/assets/framer.png",
       },
     ],
   },
   {
-    title: 'CarePulse - Health Management System',
-    desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
-    subdesc:
-      'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
-    href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
-    texture: '/textures/project/project3.mp4',
-    logo: '/assets/project-logo3.png',
+    title: "Dutiful - Business growth platform",
+    desc: `Eliminate manual work and save time by listing your business or service on the best business directory in Nigeria.
+      Dutiful is a business growth platform that accelerates your business reputation by 90% and gain social proof through feedbacks
+      requested from users.`,
+    subdesc: `Dutiful is a pet project I built with Next.js, TailwindCSS, and Framer Motion, Dutiful is a platform that helps businesses grow by collecting reviews and ratings from customers.`,
+    href: "https://dutiful-app.netlify.app/",
+    texture: "/textures/project/project3.mp4",
+    logo: "/assets/project-logo3.png",
     logoStyle: {
-      backgroundColor: '#60f5a1',
+      backgroundColor: "#a27fbe",
       background:
-        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
-      border: '0.2px solid rgba(208, 213, 221, 1)',
-      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+        "linear-gradient(0deg, #ede3f4, #ede3f4), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)",
+      border: "0.2px solid rgba(208, 213, 221, 1)",
+      boxShadow: "0px 0px 60px 0px rgba(35, 131, 96, 0.3)",
     },
-    spotlight: '/assets/spotlight3.png',
+    spotlight: "/assets/spotlight3.png",
     tags: [
       {
         id: 1,
-        name: 'React.js',
-        path: '/assets/react.svg',
+        name: "Next.js",
+        path: "/assets/nextjs.svg",
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
       },
       {
         id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
+        name: "TypeScript",
+        path: "/assets/typescript.png",
       },
       {
         id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
+        name: "Framer Motion",
+        path: "/assets/framer.png",
       },
     ],
   },
   {
-    title: 'Horizon - Online Banking Platform',
-    desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
+    title: "KardNode - Credit card for developers",
+    desc: `kardNode is a special credit card made for developers. It offers a wide range of benefits, including cashback on purchases,`,
     subdesc:
-      'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
-    href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
-    texture: '/textures/project/project4.mp4',
-    logo: '/assets/project-logo4.png',
+      `KardNode is a pet project I built with Nextjs to showcase my frontend skills.`,
+    href: "https://kardnode.netlify.app",
+    texture: "/textures/project/project4.mp4",
+    logo: "/assets/project-logo4.png",
     logoStyle: {
-      backgroundColor: '#0E1F38',
-      border: '0.2px solid #0E2D58',
-      boxShadow: '0px 0px 60px 0px #2F67B64D',
+      backgroundColor: "#0E1F38",
+      border: "0.2px solid #0E2D58",
+      boxShadow: "0px 0px 60px 0px #2F67B64D",
     },
-    spotlight: '/assets/spotlight4.png',
+    spotlight: "/assets/spotlight4.png",
     tags: [
       {
         id: 1,
-        name: 'React.js',
-        path: '/assets/react.svg',
+        name: "Next.js",
+        path: "/assets/nextjs.svg",
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
       },
       {
         id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
+        name: "TypeScript",
+        path: "/assets/typescript.png",
       },
       {
         id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
-      },
-    ],
-  },
-  {
-    title: 'Imaginify - AI Photo Manipulation App',
-    desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
-    subdesc:
-      'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
-    href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
-    texture: '/textures/project/project5.mp4',
-    logo: '/assets/project-logo5.png',
-    logoStyle: {
-      backgroundColor: '#1C1A43',
-      border: '0.2px solid #252262',
-      boxShadow: '0px 0px 60px 0px #635BFF4D',
-    },
-    spotlight: '/assets/spotlight5.png',
-    tags: [
-      {
-        id: 1,
-        name: 'React.js',
-        path: '/assets/react.svg',
-      },
-      {
-        id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
-      },
-      {
-        id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
-      },
-      {
-        id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
+        name: "Framer Motion",
+        path: "/assets/framer.png",
       },
     ],
   },
@@ -234,19 +203,43 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
   return {
     deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
     deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
-    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [11, -6.5, 0],
-    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
-    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
-    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    cubePosition: isSmall
+      ? [4, -5, 0]
+      : isMobile
+      ? [5, -5, 0]
+      : isTablet
+      ? [5, -5, 0]
+      : [11, -6.5, 0],
+    reactLogoPosition: isSmall
+      ? [3, 4, 0]
+      : isMobile
+      ? [5, 4, 0]
+      : isTablet
+      ? [5, 4, 0]
+      : [12, 3, 0],
+    ringPosition: isSmall
+      ? [-5, 7, 0]
+      : isMobile
+      ? [-10, 10, 0]
+      : isTablet
+      ? [-12, 10, 0]
+      : [-24, 10, 0],
+    targetPosition: isSmall
+      ? [-5, -10, -10]
+      : isMobile
+      ? [-9, -10, -10]
+      : isTablet
+      ? [-11, -7, -10]
+      : [-13, -13, -10],
   };
 };
 
 export const workExperiences = [
   {
     id: 1,
-    name: 'Graff Systems',
-    pos: 'Full Stack Engineer',
-    duration: '2024 - Present',
+    name: "Graff Systems",
+    pos: "Full Stack Engineer",
+    duration: "2024 - Present",
     title: `As a full-stack engineer at Graff Systems, I work on a wide range of projects, 
         from building fullstack school management portal to varieties of client websites. 
         I used technologies such as React.js, Next.js, Framer Motion, Tailwindcss, Node.js, Nest.js, and databases like PostgreSQL 
@@ -255,37 +248,37 @@ export const workExperiences = [
     technologies: [
       {
         id: 2,
-        name: 'Next.js',
+        name: "Next.js",
       },
       {
         id: 3,
-        name: 'Framer Motion',
+        name: "Framer Motion",
       },
       {
         id: 4,
-        name: 'Tailwind CSS',
+        name: "Tailwind CSS",
       },
       {
         id: 6,
-        name: 'Nest.js',
+        name: "Nest.js",
       },
       {
         id: 7,
-        name: 'PostgreSQL',
+        name: "PostgreSQL",
       },
       {
         id: 8,
-        name: 'MongoDB',
+        name: "MongoDB",
       },
     ],
-    icon: '/assets/graff_logo.jpeg',
-    animation: 'victory',
+    icon: "/assets/graff_logo.jpeg",
+    animation: "victory",
   },
   {
     id: 2,
-    name: 'Trudenty',
-    pos: 'Frontend Developer',
-    duration: '2022 - 2024',
+    name: "Trudenty",
+    pos: "Frontend Developer",
+    duration: "2022 - 2024",
     title: `At Trudenty, I worked as a frontend developer, 
         building pixel-perfect user interfaces and a b2b web application that served businesses in the
         identity verification space. The application enabled them to customize their verification process,
@@ -295,34 +288,33 @@ export const workExperiences = [
     technologies: [
       {
         id: 1,
-        name: 'React.js',
+        name: "React.js",
       },
       {
         id: 2,
-        name: 'Tailwind CSS',
+        name: "Tailwind CSS",
       },
       {
         id: 3,
-        name: 'Gatsby.js',
+        name: "Gatsby.js",
       },
       {
         id: 4,
-        name: 'Docusaurus',
+        name: "Docusaurus",
       },
       {
         id: 5,
-        name: 'RTK Query',
+        name: "RTK Query",
       },
-
     ],
-    icon: '/assets/trudenty_logo.jpeg',
-    animation: 'clapping',
+    icon: "/assets/trudenty_logo.jpeg",
+    animation: "clapping",
   },
   {
     id: 3,
-    name: 'Konnected',
-    pos: 'Frontend Developer',
-    duration: '2022 - 2022',
+    name: "Konnected",
+    pos: "Frontend Developer",
+    duration: "2022 - 2022",
     title: `At Konnected, I worked as a frontend developer, 
         building an SEO friendly web application with Next.js that featured audio recording, 
         transcript generation, audio sharing with embedded social links.
@@ -330,50 +322,50 @@ export const workExperiences = [
     technologies: [
       {
         id: 1,
-        name: 'Next.js',
+        name: "Next.js",
       },
       {
         id: 2,
-        name: 'Tailwind CSS',
+        name: "Tailwind CSS",
       },
       {
         id: 3,
-        name: 'RTK Query',
-      }
+        name: "RTK Query",
+      },
     ],
-    icon: '/assets/feelkonnected_logo.jpeg',
-    animation: 'salute',
+    icon: "/assets/feelkonnected_logo.jpeg",
+    animation: "salute",
   },
   {
     id: 4,
-    name: 'Kiddiebox',
-    pos: 'Frontend Developer',
-    duration: '2022 - 2022',
+    name: "Kiddiebox",
+    pos: "Frontend Developer",
+    duration: "2022 - 2022",
     title: `At Kiddiebox, I worked as a frontend developer,
         revamping and managing a childcare management application for childcare providers and parents. 
     `,
     technologies: [
       {
         id: 1,
-        name: 'React.js',
+        name: "React.js",
       },
       {
         id: 2,
-        name: 'CSS',
+        name: "CSS",
       },
       {
         id: 3,
-        name: 'Redux',
-      }
+        name: "Redux",
+      },
     ],
-    icon: '/assets/getkiddiebox_logo.jpeg',
-    animation: 'idle',
+    icon: "/assets/getkiddiebox_logo.jpeg",
+    animation: "idle",
   },
   {
     id: 5,
-    name: 'Z-Softbase Software Solutions',
-    pos: 'Full Stack Engineer',
-    duration: '2020 - 2021',
+    name: "Z-Softbase Software Solutions",
+    pos: "Full Stack Engineer",
+    duration: "2020 - 2021",
     title: `
       At Z-Softbase Software Solutions, I worked as a fullstack engineer,
       building a school management portal that enabled schools to manage their students, teachers, and subjects.
@@ -381,22 +373,22 @@ export const workExperiences = [
     technologies: [
       {
         id: 1,
-        name: 'React.js',
+        name: "React.js",
       },
       {
         id: 2,
-        name: 'React Native',
+        name: "React Native",
       },
       {
         id: 3,
-        name: 'Express.js',
+        name: "Express.js",
       },
       {
         id: 4,
-        name: 'MongoDB',
-      } 
+        name: "MongoDB",
+      },
     ],
-    icon: 'https://www.placeholder.com/150',
-    animation: 'idle',
+    icon: "https://www.placeholder.com/150",
+    animation: "idle",
   },
 ];
